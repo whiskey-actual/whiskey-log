ada92a7e4a557804291220493764a7458ebcacc7
Whiskey Actual <actualwhiskey@gmail.com>
2024-08-21 07:53:17 -0400

    tweak

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export class LogEngine {
   public AddLogEntry(type:LogEntryType, message:string|string[], preceedingBlankLine:boolean=false, subsequentBlankLine:boolean=false) {
     
     try {
-      if(message && message.length>0 && (type!=LogEntryType.Debug || (type===LogEntryType.Debug && this._showDebug))) {
+      if(message && (type!=LogEntryType.Debug || (type===LogEntryType.Debug && this._showDebug))) {
 
         let entryColorSequence = FgWhite
         let entryColorText = '';
